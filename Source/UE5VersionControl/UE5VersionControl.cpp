869eeb90d4c81009896372332d9a267777632dcd
Topi Ilmoni <topi.ilmoni@gmail.com>
2023-09-17 18:56:05 +0300

    Added ActorSupreme

---
+++ Source/UE5VersionControl/UE5VersionControl.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE5VersionControl.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5VersionControl, "UE5VersionControl" );
